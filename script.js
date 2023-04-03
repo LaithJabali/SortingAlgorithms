d0da5888d4513204be688db512286236b97b2de8
LaithJabali <laithkingjabali@gmail.com>
2023-04-03 22:46:41 +0200

    finale

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const arrayContainer = document.getElementById('array-container');
-const randomizeBtn = document.getElementById('randomize-btn');
-const solveBtn = document.getElementById('solve-btn');
 const arrayLengthInput = document.getElementById('array-length');
+const randomizeBtn = document.getElementById('randomize-btn');
+const bubble = document.getElementById('bubble-sort-btn');
+const selection = document.getElementById('selection-sort-btn');
+const insertion = document.getElementById('insertion-sort-btn');
+const mergeS = document.getElementById('merge-sort-btn');
+const quickS = document.getElementById('quick-sort-btn');
 let array = [];
 
-//to generate a random integer
+
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-//to create the array elements
 function displayArray() {
   arrayContainer.innerHTML = '';
   for (let i = 0; i < array.length; i++) {
@@ -21,7 +24,6 @@ function displayArray() {
   }
 }
 
-//to randomize the array
 function randomizeArray() {
   const length = arrayLengthInput.value;
   array = [];
@@ -41,13 +43,114 @@ function swap(array, i, j) {
         if (array[j] > array[j + 1]) {
           swap(array, j, j + 1);
           displayArray();
-          // delay for visualization
           await new Promise(resolve => setTimeout(resolve, 100)); 
         }
       }
     }
 }
 
-// event listener for the randomize button
+async function selectionSort() {
+    for (let i = 0; i < array.length; i++) {
+      let minIndex = i;
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] < array[minIndex]) {
+          minIndex = j;
+        }
+      }
+      if (minIndex !== i) {
+        swap(array, i, minIndex);
+        displayArray();
+        await new Promise(resolve => setTimeout(resolve, 100));
+      }
+    }
+  }
+
+  async function insertionSort() {
+    for (let i = 1; i < array.length; i++) {
+      let key = array[i];
+      let j = i - 1;
+      while (j >= 0 && array[j] > key) {
+        array[j + 1] = array[j];
+        j = j - 1;
+        displayArray();
+        await new Promise(resolve => setTimeout(resolve, 100));
+      }
+      array[j + 1] = key;
+      displayArray();
+      await new Promise(resolve => setTimeout(resolve, 100));
+    }
+  }
+  
+
+  async function mergeSort() {
+    array = await mergeSortHelper(array);
+    displayArray();
+  }
+  
+  async function mergeSortHelper(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+  
+    return merge(await mergeSortHelper(left), await mergeSortHelper(right));
+  }
+  
+  async function merge(left, right) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < left.length && j < right.length) {
+      if (left[i] < right[j]) {
+        result.push(left[i]);
+        i++;
+      } else {
+        result.push(right[j]);
+        j++;
+      }
+      displayArray();
+      await new Promise(resolve => setTimeout(resolve, 100));
+    }
+    return result.concat(left.slice(i)).concat(right.slice(j));
+  }
+  
+  async function quickhelper() {
+    await quickSort(0, array.length - 1);
+  }
+  async function quickSort(low, high) {
+    if (low < high) {
+      const partitionIndex = await partition(low, high);
+      await quickSort(low, partitionIndex - 1);
+      await quickSort(partitionIndex + 1, high);
+    }
+  }
+  
+  async function partition(low, high) {
+    const pivot = array[high];
+    let i = low - 1;
+  
+    for (let j = low; j < high; j++) {
+      if (array[j] < pivot) {
+        i++;
+        swap(array, i, j);
+        displayArray();
+        await new Promise(resolve => setTimeout(resolve, 100)); 
+      }
+    }
+  
+    swap(array, i + 1, high);
+    displayArray();
+    await new Promise(resolve => setTimeout(resolve, 100)); 
+  
+    return i + 1;
+  }
+  
+
 randomizeBtn.addEventListener('click', randomizeArray);
-solveBtn.addEventListener('click', bubbleSort);
+bubble.addEventListener('click', bubbleSort);
+selection.addEventListener('click', selectionSort);
+insertion.addEventListener('click', insertionSort);
+mergeS.addEventListener('click', mergeSort);
+quickS.addEventListener('click', quickhelper);
